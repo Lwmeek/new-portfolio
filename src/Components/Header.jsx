aa4ec1a2c734820e4f3f225af836de0e137f22d0
Timic Ivey <timicivey27@gmail.com>
2023-10-05 12:54:04 -0400

    adding icons for github and linkedin

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./Navbar";
-import { BsGithub } from "react-icons/bs";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Header = () => {
 	return (
@@ -12,7 +12,10 @@ const Header = () => {
 					src="./src/images/jags banner.jpg"
 					alt="jaguars banner"
 				/>
-                <BsGithub/> GitHub
+                <div className="icon-group">
+                <a className=" btn btn-sm btn-link text-lg font-bold mb-4"><BsGithub className="icon text-base-content"/></a>
+                <a className=" btn btn-sm btn-link text-lg font-bold"><BsLinkedin className="icon text-base-content"/></a>
+                </div>
 			</header>
 		</>
 	);
