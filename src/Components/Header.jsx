b5615f449d22b2e1b57284a252acff5db57ee677
Timic Ivey <timicivey27@gmail.com>
2023-10-07 04:47:27 -0400

    adding swap and starting menu links

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,10 +12,14 @@ const Header = () => {
 					src="./src/images/jags banner.jpg"
 					alt="jaguars banner"
 				/>
-                <div className="icon-group">
-                <a className=" btn btn-sm btn-link text-lg font-bold mb-4"><BsGithub className="icon text-base-content"/></a>
-                <a className=" btn btn-sm btn-link text-lg font-bold"><BsLinkedin className="icon text-base-content"/></a>
-                </div>
+				<div className="icon-group">
+					<a className=" btn btn-sm btn-link text-lg font-bold mb-4">
+						<BsGithub className="icon text-base-content" />
+					</a>
+					<a className=" btn btn-sm btn-link text-lg font-bold">
+						<BsLinkedin className="icon text-base-content" />
+					</a>
+				</div>
 			</header>
 		</>
 	);
