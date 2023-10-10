64772f70b5a90dab2a25e16a09d6a53cf3dca5e2
Timic Ivey <timicivey27@gmail.com>
2023-10-10 15:22:47 -0400

    Project section starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./Components/Header/Header";
 import Introduction from "./Components/Introduction/Introduction";
+import Projects from "./Components/Projects/Projects";
 import Skills from "./Components/Skills/skills";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 			<Header />
 			<Introduction />
 			<Skills />
+			<Projects />
 		</>
 	);
 }
---
+++ src/Components/Projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Projects = () => {
+	return (
+		<div className="tabs">
+			<a className="tab tab-lifted">Project 1</a>
+			<a className="tab tab-lifted tab-active">Project 2</a>
+			<a className="tab tab-lifted">Project 3</a>
+		</div>
+	);
+};
+
+export default Projects;
