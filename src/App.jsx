43c6c64699f78aa5532770227274aa711f49ca75
Timic Ivey <timicivey27@gmail.com>
2023-10-10 15:16:34 -0400

    Skills section starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Components/Header/Header";
 import Introduction from "./Components/Introduction/Introduction";
+import Skills from "./Components/Skills/skills";
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Introduction />
+			<Skills />
 		</>
 	);
 }
---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Skills = () => {
+	return (
+		<>
+			<h1 className="text-4xl">Skills I Possess</h1>
+			<p>Frontend Development</p>
+			<p>HTML/CSS/JavaScript</p>
+			<p>React.js</p>
+			<p>SASS</p>
+			<p>Tailwind CSS</p>
+			<p>Vite</p>
+			<p>Chakra UI/Material UI/Daisy UI</p>
+			<p>Git/GitHub</p>
+			<p>VS Code</p>
+			<p>Restful Apis</p>
+			<p>Bootstrap</p>
+			<p>Vercel/Netlify</p>
+			<p>Communication</p>
+		</>
+	);
+};
+
+export default Skills;
