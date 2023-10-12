7b8270dbde902c7697b9c5f1be3ce16e8a7e69a5
Timic Ivey <timicivey27@gmail.com>
2023-10-12 15:25:31 -0400

    updating display of intro section

--- src/Components/Introduction/Introduction.jsx
+++ src/Components/Introduction/Introduction.jsx
@@ -8,12 +8,30 @@ const Introduction = () => {
 					src="./src/images/zenitsu.png"
 					className="max-w-sm rounded-lg shadow-2xl"
 				/>
-				<div>
-					<p>Age: 27</p>
-					<p className="py-6">Email: timicivey27@gmail.com</p>
-					<p className="py-6">Phone: (386)-631-4549</p>
-					<p className="py-6">Location: Jacksonville, Florida, USA</p>
-					<button className="btn btn-primary">Resume</button>
+				<section className="intro p-4">
+					I'm <span>Timic Ivey</span> a self-taught frontend developer certified
+					in HTML/CSS/JavaScript, React.js, and currently learning SASS and
+					Vue.js. I love gaming and watching sports in my free time while
+					balancing it with my passion for evolving the skills of my craft,
+					frontend development.
+				</section>
+				<div className="grid grid-cols-2 items-center justify-items-center">
+					<p>
+						<span>Age:</span> 27
+					</p>
+					<p className="py-6">
+						<span>Location:</span> Jacksonville, Florida, USA
+					</p>
+					<p className="py-6">
+						<span>Email:</span> timicivey27@gmail.com
+					</p>
+					<p className="py-6">
+						<span>Phone:</span> (386)-631-4549
+					</p>
+
+					<button className="btn btn-primary col-span-2 w-56 justify-self-center">
+						Resume
+					</button>
 				</div>
 			</div>
 		</div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Projects = () => {
 	return (
-		<div className="tabs">
+		<div className="tabs bg-neutral">
 			<a className="tab tab-lifted">Project 1</a>
 			<a className="tab tab-lifted tab-active">Project 2</a>
 			<a className="tab tab-lifted">Project 3</a>
