a95f54bf46a1448d16b7accfa8a5ef950ee97f5b
Timic Ivey <timicivey27@gmail.com>
2023-10-17 03:46:44 -0400

    skills into badges

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -6,20 +6,26 @@ const Skills = () => {
 			<section className="skills-section p-5 mb-16">
 				<h1 className="text-4xl mb-8">Skills I Possess</h1>
 				<div className="grid grid-cols-2 gap-3">
-					<p>Frontend Development</p>
-					<p>HTML/CSS/JavaScript</p>
-					<p>React.js</p>
-					<p>SASS</p>
-					<p>Tailwind CSS</p>
-					<p>Vite</p>
-					<p>Webpack</p>
-					<p>Chakra UI/Material UI/Daisy UI</p>
-					<p>Git/GitHub</p>
-					<p>VS Code</p>
-					<p>Restful Apis</p>
-					<p>Bootstrap</p>
-					<p>Vercel/Netlify</p>
-					<p>Communication</p>
+					<p className="badge badge-accent text-secondary">
+						Frontend Development
+					</p>
+					<p className="badge badge-accent text-secondary">
+						HTML/CSS/JavaScript
+					</p>
+					<p className="badge badge-accent text-secondary">React.js</p>
+					<p className="badge badge-accent text-secondary">SASS</p>
+					<p className="badge badge-accent text-secondary">Tailwind CSS</p>
+					<p className="badge badge-accent text-secondary">Vite</p>
+					<p className="badge badge-accent text-secondary">Webpack</p>
+					<p className="badge badge-accent text-secondary">
+						Chakra UI/Material UI/Daisy UI
+					</p>
+					<p className="badge badge-accent text-secondary">Git/GitHub</p>
+					<p className="badge badge-accent text-secondary">VS Code</p>
+					<p className="badge badge-accent text-secondary">Restful Apis</p>
+					<p className="badge badge-accent text-secondary">Bootstrap</p>
+					<p className="badge badge-accent text-secondary">Vercel/Netlify</p>
+					<p className="badge badge-accent text-secondary">Communication</p>
 				</div>
 			</section>
 		</>
