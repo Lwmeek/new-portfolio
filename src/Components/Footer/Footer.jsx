6fc57ee37edd3f7c524497717396c04c077ddfb9
Timic Ivey <timicivey27@gmail.com>
2023-11-10 11:15:51 -0500

    update footer icon size

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
 	return (
 		<section className="bg-neutral pt-8 pb-8">
 			<div className="text-center mb-4">
-				<a className="btn btn-xs btn-link text-lg font-bold mr-4">
+				<a className="btn btn-xs btn-link text-3xl font-bold mr-4">
 					<BsGithub className="icon text-secondary" />
 				</a>
-				<a className="btn btn-xs btn-link text-lg font-bold">
+				<a className="btn btn-xs btn-link text-3xl font-bold">
 					<BsLinkedin className="icon text-secondary" />
 				</a>
 			</div>
