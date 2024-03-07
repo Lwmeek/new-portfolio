152ba9dc7c426e4e12ea5fc71a2f2cac0c19ce7c
Timic Ivey <timicivey27@gmail.com>
2024-03-07 16:06:26 -0500

    updated descriptions for projects

--- src/assets/Useful Data/ProjectArr.jsx
+++ src/assets/Useful Data/ProjectArr.jsx
@@ -3,7 +3,8 @@ export const ProjectsArr = [
 		id: 0,
 		img: "./src/images/Video Game Hub.png",
 		title: "Video Game Hub",
-		description: "If a dog chews shoes whose shoes does he choose?",
+		description:
+			"This project is a Gaming hub displaying different games on different systems, different categories, and with ratings. Using an API from Rawg I was able to fetch different game titles with details such as the ratings. A search bar was also added for clients to be able to search for those titles they seek. I implemented a dark mode as well which helped me learn more about working with color themes with chakra ui. Using chakra ui for my styles, got me to learn a new library as i was making this app. Learning on top of learning while building is always a plus! This project allowed me to also increase my JavaScript knowledge as new/different tasks showed itself while building. I loved this project as it allowed me to combine my passion for frontend development and gaming.",
 		link: "https://video-game-hub-il1q.vercel.app/",
 		badges: [
 			"VS Code",
@@ -19,7 +20,8 @@ export const ProjectsArr = [
 		id: 1,
 		img: "./src/images/Social Media Dashboard dark theme.png",
 		title: "Social Media Dashboard",
-		description: "If a dog chews shoes whose shoes does he choose?",
+		description:
+			"Using the great languages of HTML/CSS and Javascript, I made a stats Dashboard display. I incuded some gradients, a dark and light mode, switch button, and nice details. The next level for this project I would say is using an API to get all of the stats information for each card.",
 		link: "https://amazing-boba-8f87e3.netlify.app/",
 		badges: ["VS Code", "HTML/CSS", "JavaScript", "Netlify"],
 	},
@@ -27,23 +29,9 @@ export const ProjectsArr = [
 		id: 2,
 		img: "./src/images/Chat App.png",
 		title: "Chat App",
-		description: "If a dog chews shoes whose shoes does he choose?",
+		description:
+			"One of my first projects starting out. I used purely HTML and CSS to create the shapes and structure to make the display you see in the photo. It quickly allowed me to understand CSS/HTML on a deeper level, shwoing me the power it holds.",
 		link: "https://celadon-chaja-ad048a.netlify.app/",
 		badges: ["VS Code", "HTML/CSS", "Netlify"],
-	},
-	{
-		id: 3,
-		img: "./src/images/zenitsu.png",
-		title: "No",
-		description: "If a dog chews shoes whose shoes does he choose?",
-		link: "hi",
-		badges: [
-			"VS Code",
-			"React.js",
-			"Vite",
-			"Tailwind CSS",
-			"Daisy UI",
-			"Vercel",
-		],
 	},
 ];
